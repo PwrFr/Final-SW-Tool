954b03f4d5aa7bf209ca7ed7f9e6ac98a690aac2
PwrFr <54875724+PwrFr@users.noreply.github.com>
2022-05-10 14:42:01 +0700

    gitignore

--- backend/index.js
+++ backend/index.js
@@ -8,32 +8,57 @@ app.use(cors())
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
 
 const { MongoClient } = require("mongodb");
-const uri = "mongodb://myUserAdmin:myUserAdmin@localhost:27017";
+const uri = "mongodb+srv://pwr:CKJxki16@swt.2utpf.mongodb.net/test";
 
-app.post('/users/create', async(req, res) => {
+app.post('/users/create', async (req, res) => {
   const user = req.body;
   const client = new MongoClient(uri);
   await client.connect();
   await client.db('mydb').collection('users').insertOne({
     id: parseInt(user.id),
+    vaccine: user.vaccine,
+    pname: user.pname,
     fname: user.fname,
     lname: user.lname,
-    username: user.username,
-    email: user.email,
-    avatar: user.avatar
+    type_identity: user.type_identity,
+    identity: user.identity,
+    dob: user.dob,
+    address: user.address,
+    province: user.province,
+    district: user.district,
+    zone: user.zone,
+    phone: user.phone
+
   });
   await client.close();
   res.status(200).send({
     "status": "ok",
-    "message": "User with ID = "+user.id+" is created",
+    "message": "User with ID = " + user.id + " is created",
+    "user": user
+  });
+})
+
+app.post('/appointment', async (req, res) => {
+  const user = req.body;
+  const client = new MongoClient(uri);
+  await client.connect();
+  await client.db('mydb').collection('appointment').insertOne({
+    id: parseInt(user.id),
+    date: user.date
+
+  });
+  await client.close();
+  res.status(200).send({
+    "status": "ok",
+    "message": "Your ID is " + user.id + " Your Appointment is " + user.date,
     "user": user
   });
 })
